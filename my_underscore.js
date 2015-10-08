3ee210d8ef4e19bfd28652bdaf91f7acc0665b7c
wangxu <ttian226@gmail.com>
2015-10-08 16:51:11 +0800

    u

---
+++ my_underscore.js
@@ -0,0 +1,158 @@
+(function () {
+    // 创建root对象，在浏览器中指向的是`window`对象
+    var root = this;
+
+    // 声明变量保存前缀`_`
+    var previousUnderscore = root._;
+
+    // 保存原型对象的引用
+    var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
+
+    // 创建快速的核心原型方法的引用
+    var
+        push = ArrayProto.push,
+        slice = ArrayProto.slice,
+        toString = ObjProto.toString,
+        hasOwnProperty = ObjProto.hasOwnProperty;
+
+    // 声明**ECMAScript 5**我们能够使用到的一些本地函数的引用
+    var
+        nativeIsArray = Array.isArray,
+        nativeKeys = Object.keys,
+        nativeBind = FuncProto.bind,
+        nativeCreate = Object.create;
+
+    var Ctor = function () {};
+
+    // 创建一个安全的underscore对象的引用
+    var _ = function (obj) {
+        if (obj instanceof _) {
+            return obj;
+        }
+
+        if (!(this instanceof _)) {
+            return new _(obj);
+        }
+
+        this._wrapped = obj;
+    };
+
+    // 如果浏览器下添加`_`作为全局对象
+    if (typeof exports !== 'undefined') {
+
+    } else {
+        root._ = _;
+    }
+
+    // 当前版本号
+    _.VERSION = '1.8.3';
+
+    var optimizeCb = function (func, context, argCount) {
+        if (context === void 0) {
+            return func;
+        }
+    };
+
+    // 在内部经常使用的一个函数，产生一个回调函数用来迭代集合中每一个元素
+    var cb = function (value, context, argCount) {
+        if (_.isFunction(value)) {
+            return optimizeCb(value, context, argCount);
+        }
+    };
+
+    var property = function (key) {
+        return function (obj) {
+            return obj == null ? void 0 : obj[key];
+        };
+    };
+
+    // 定义collection的辅助方法
+    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+    var getLength = property('length');
+    var isArrayLike = function (collection) {
+        var length = getLength(collection);
+        return typeof length === 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+    };
+
+    // Collection Functions
+    // 遍历集合
+    _.each = _.forEach = function (obj, iteratee, context) {
+        iteratee = optimizeCb(iteratee, context);
+        var i, length;
+        if (isArrayLike(obj)) {
+            for (i = 0, length = obj.length; i < length; i++) {
+                iteratee(obj[i], i, obj);
+            }
+        } else {
+            var keys = _.keys(obj);
+            for (i = 0, length = keys.length; i < length; i++) {
+                iteratee(obj[keys[i]], keys[i], obj);
+            }
+        }
+        return obj;
+    };
+
+    // 映射集合元素，返回一个新的数组
+    _.map = _.collect = function (obj, iteratee, context) {
+        iteratee = cb(iteratee, context);
+        var keys = !isArrayLike(obj) && _.keys(obj),
+            length = (keys || obj).length,
+            results = Array(length);
+
+        for (var index = 0; index < length; index++) {
+            var currentKey = keys ? keys[index] : index;
+            results[index] = iteratee(obj[currentKey], currentKey, obj);
+        }
+        return results;
+    };
+
+    // 创建一个reduce方法，从左或从右迭代
+    function createReduce(dir) {
+
+        function iterator(obj, iteratee, memo, keys, index, length) {
+
+        }
+
+        return function (obj, iteratee, memo, context) {
+
+        };
+    }
+
+    // Object Functions
+
+    // 检索对象的属性名，返回一个数组，**ECMAScript 5**使用本地方法`Object.keys`返回
+    _.keys = function (obj) {
+        if (!_.isObject(obj)) {
+            return [];
+        }
+
+        if (nativeKeys) {
+            return nativeKeys(obj);
+        }
+
+        var keys = [];
+        for (var key in obj) {
+            if (_.has(obj, key)) {
+                keys.push(key);
+            }
+        }
+        return keys;
+    };
+
+    // 检查给定的值是否是对象
+    _.isObject = function (obj) {
+        var type = typeof obj;
+        return type === 'function' || type === 'object' && !!obj;
+    };
+
+    // 检查对象是否是函数类型
+    _.isFunction = function (obj) {
+        return typeof obj == 'function' || false;
+    };
+
+    // 检查对象是否有指定的给定的直接属性
+    _.has = function (obj, key) {
+        return obj != null && hasOwnProperty.call(obj, key);
+    };
+
+}.call(this));
